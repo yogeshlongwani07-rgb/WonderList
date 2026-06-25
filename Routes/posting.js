0822ce734732a85f12d41deabd44ab4f3e33ddcb
Yogesh Longwani <yogeshlongwani07@gmail.com>
2026-06-26 00:41:39 +0530

    Add isLoggedIn middleware to posting route

--- Routes/posting.js
+++ Routes/posting.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
 const AsyncWrap = require("../utils/Wrapasync");
-const {validateListing} = require("../middleware");
+const {validateListing , isLoggedIn} = require("../middleware");
 const routeController = require("../controllers/listings");
 
 //Add listing to listings
